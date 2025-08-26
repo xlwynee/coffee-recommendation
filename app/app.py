e04bf47c344bfe565c8c5cc0c9d1cdc76619e432
Rand Otb <randotb@Rands-MacBook-Pro.local>
2025-08-26 22:32:12 +0300

    Prepare for Heroku deploy

--- app/app.py
+++ app/app.py
@@ -163,5 +163,7 @@ def index():
 
     return render_template("index.html", recommendations=recommendations)
 
+
 if __name__ == "__main__":
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
